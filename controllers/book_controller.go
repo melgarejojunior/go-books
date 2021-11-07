24f44cfd67caf88dbe029a356d40324b1c87603f
Júnior Melgarejo <joao.melgarejo@picpay.com>
2021-11-07 19:50:23 -0400

    Improving code

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -8,13 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func ShowBook(c *gin.Context) {
+type response struct {
+	*gin.Context
+}
+
+func ShowBook(context *gin.Context) {
+	c := response{context}
 	id := c.Param("id")
 
 	newid, err := strconv.Atoi(id)
 
 	if err != nil {
-		emitError(c, 422, "ID has to be integer")
+		c.emitError(422, "ID has to be integer")
 		return
 	}
 
@@ -24,14 +29,15 @@ func ShowBook(c *gin.Context) {
 	err = db.First(&book, newid).Error
 
 	if err != nil {
-		emitError(c, 400, "Book not found. "+err.Error())
+		c.emitError(400, "Book not found. "+err.Error())
 		return
 	}
 
-	emitSuccess(c, book)
+	c.emitSuccess(book)
 }
 
-func CreateBook(c *gin.Context) {
+func CreateBook(context *gin.Context) {
+	c := response{context}
 	db := database.GetDatabase()
 
 	var book models.Book
@@ -39,21 +45,22 @@ func CreateBook(c *gin.Context) {
 	err := c.ShouldBindJSON(&book)
 
 	if err != nil {
-		emitError(c, 422, "Wrong parameters. "+err.Error())
+		c.emitError(422, "Wrong parameters. "+err.Error())
 		return
 	}
 
 	err = db.Create(&book).Error
 
 	if err != nil {
-		emitError(c, 400, "Something went wrong. "+err.Error())
+		c.emitError(400, "Something went wrong. "+err.Error())
 		return
 	}
 
-	emitSuccess(c, book)
+	c.emitSuccess(book)
 }
 
-func ShowBooks(c *gin.Context) {
+func ShowBooks(context *gin.Context) {
+	c := response{context}
 	db := database.GetDatabase()
 
 	var books []models.Book
@@ -61,14 +68,15 @@ func ShowBooks(c *gin.Context) {
 	err := db.Find(&books).Error
 
 	if err != nil {
-		emitError(c, 400, "Cannot list books. "+err.Error())
+		c.emitError(400, "Cannot list books. "+err.Error())
 		return
 	}
 
-	emitSuccess(c, books)
+	c.emitSuccess(books)
 }
 
-func UpdateBook(c *gin.Context) {
+func UpdateBook(context *gin.Context) {
+	c := response{context}
 	db := database.GetDatabase()
 
 	var book models.Book
@@ -76,27 +84,28 @@ func UpdateBook(c *gin.Context) {
 	err := c.ShouldBindJSON(&book)
 
 	if err != nil {
-		emitError(c, 422, "Wrong parameters. "+err.Error())
+		c.emitError(422, "Wrong parameters. "+err.Error())
 		return
 	}
 
 	err = db.Save(&book).Error
 
 	if err != nil {
-		emitError(c, 400, "Something went wrong. "+err.Error())
+		c.emitError(400, "Something went wrong. "+err.Error())
 		return
 	}
 
-	emitSuccess(c, book)
+	c.emitSuccess(book)
 }
 
-func DeleteBook(c *gin.Context) {
+func DeleteBook(context *gin.Context) {
+	c := response{context}
 	id := c.Param("id")
 
 	newid, err := strconv.Atoi(id)
 
 	if err != nil {
-		emitError(c, 422, "ID has to be integer")
+		c.emitError(422, "ID has to be integer")
 		return
 	}
 
@@ -105,19 +114,19 @@ func DeleteBook(c *gin.Context) {
 	err = db.Delete(&models.Book{}, newid).Error
 
 	if err != nil {
-		emitError(c, 400, "Cannot delete this book. "+err.Error())
+		c.emitError(400, "Cannot delete this book. "+err.Error())
 		return
 	}
 
 	c.Status(204)
 }
 
-func emitError(c *gin.Context, code int, errorStr string) {
+func (c response) emitError(code int, errorStr string) {
 	c.JSON(code, gin.H{
 		"error": errorStr,
 	})
 }
 
-func emitSuccess(c *gin.Context, a interface{}) {
+func (c response) emitSuccess(a interface{}) {
 	c.JSON(200, a)
 }
